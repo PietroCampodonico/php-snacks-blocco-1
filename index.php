b47f27d0488994b5dd62c7baccb7fdc84fd7c91f
PietroCampodonico <pietro.campodonico@outlook.it>
2021-05-31 17:53:27 +0200

    Snack #2 completato

--- index.php
+++ index.php
@@ -19,7 +19,39 @@ $tappeStagione = ["Venezia", "Virtus Bologna", "Brindisi", "Trento", "Dinamo Sas
 
         $punteggioPartita = rand(9, 62) . "-" . rand(9, 62);
 
-       echo "<li><strong>" . $squadraCasa . " - " . $tappeStagione[$i] . " | " . $punteggioPartita. "</li>";
+        echo "<li><strong>" . $squadraCasa . " - " . $tappeStagione[$i] . " | " . $punteggioPartita . "</li>";
     }
     ?>
+</ul>
+
+
+<?php
+## Snack 2
+/*Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo
+nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e
+una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, 
+altrimenti “Accesso negato”*/
+
+$userName = $_GET["name"];
+
+$userEmail = $_GET["mail"];
+
+$userAge = $_GET["age"];
+
+$access = "Accesso negato";
+
+?>
+
+<h1>Snack # 2</h1>
+
+<ul style="list-style: none;">
+    <?php
+    if (strlen($userName) > 3 && strpos($userEmail, "@") && strpos($userEmail, ".") && is_numeric($userAge)) {
+        $access = "Accesso consentito";
+    }
+    ?>
+    <li><?php echo strtoupper($access) ?></li>
+    <li>Nome utente: <?php echo $userEmail ?></li>
+    <li>Età utente: <?php echo $userAge ?></li>
+    <li>Mail utente: <?php echo $userName ?></li>
 </ul>
